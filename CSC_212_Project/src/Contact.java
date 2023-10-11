d25b8c067cc5ddc1772145034e3ecde8aeb12e39
SalehAD78 <145998115+SalehAD78@users.noreply.github.com>
2023-10-11 19:47:46 +0300

    Contact attributes

--- CSC_212_Project/src/Contact.java
+++ CSC_212_Project/src/Contact.java
@@ -1,8 +1,61 @@
 
-public class Contact {
+public class Contact implements Comparable{
 private String name;
+private String notes;
+private String address;
+private int phone_number;
+private String email_address;
+private String birthday;
 
 
+public Contact(String name, String notes, String address, int phone_number, String email_address, String birthday) {
+	
+	this.name = name;
+	this.notes = notes;
+	this.address = address;
+	this.phone_number = phone_number;
+	this.email_address = email_address;
+	this.birthday = birthday;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public int compareTo(Object o) {
+	
+	return 0;
+}
 
 
 }
