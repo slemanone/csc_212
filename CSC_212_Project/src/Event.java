988d1d44d5ee6492b9f51092cfcd358266edeabf
slemanone <146531001+slemanone@users.noreply.github.com>
2023-10-13 14:02:15 +0300

    Update Event.java

--- CSC_212_Project/src/Event.java
+++ CSC_212_Project/src/Event.java
@@ -1,11 +1,50 @@
 
 public class Event implements Comparable<Event>{
+	private String contactName;
+	private String eventTitle;
+	private String date_time;
+	private String location;
+
 
 	@Override
 	public int compareTo(Event o) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
+
+	
+	public String getContactName() {
+		return contactName;
+	}
+
+	public void setContactName(String contactName) {
+		this.contactName = contactName;
+	}
+
+	public String getEventTitle() {
+		return eventTitle;
+	}
+
+	public void setEventTitle(String eventTitle) {
+		this.eventTitle = eventTitle;
+	}
+
+	public String getDate_time() {
+		return date_time;
+	}
+
+	public void setDate_time(String date_time) {
+		this.date_time = date_time;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
 
 	
 
