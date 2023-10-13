c0aa341c6b62be0982d3b332acdc30f4d370ad3c
SalehAD78 <145998115+SalehAD78@users.noreply.github.com>
2023-10-13 16:32:00 +0300

    Update LinkedList.java
    We added remove method and updated the insert method

--- CSC_212_Project/src/LinkedList.java
+++ CSC_212_Project/src/LinkedList.java
@@ -1,9 +1,9 @@
-
 public class LinkedList<T extends Comparable<T>> {
 	
 private Node<T> head;
 private Node<T> current;
- 
+
+
 public LinkedList() {
 	head = current = null;
 }
@@ -13,7 +13,8 @@ public boolean full() {
 }
 
 public T retrive() {
-	 return current.data;	 }
+	 return current.data;	 
+}
 
 
 public void findfirst() {
@@ -31,49 +32,80 @@ public boolean last() {
 
 public void insertSortred(T val) {
 	Node <T> tmp;
+	Node <T> prevoius = null ;
 	if(empty()) { 
 		current = head =new Node<T> (val);	
 	}
-	else {
+	else {  // insertion at first
 		if(head.getData().compareTo(val)>0) {
 			tmp= new Node(val);
 		    tmp.setNext(head);
 		    head=tmp;	
 		}
-		else {
-			if(head.getData().compareTo(val)<0) {
-				tmp= new Node(val);
-				current = head.next;
-				head.next =tmp;
-				tmp.setNext(current);	
+		else {   // insertion at middle
+			current = head;
+			while(current!=null && current.getData().compareTo(val)<=0) {
+				prevoius = current;
+				current = current.getNext();
 			}
-			else {
-				if(head.getData().compareTo(val)== 0) {
-					tmp=new Node(val);
-					current = head.next;
-					head.next =tmp;
-					tmp.setNext(current);
+			tmp = new Node(val);
+			prevoius.next= tmp;
+			tmp.setNext(current);
+			current = tmp;		
+		}
+		 // insertion at the end when current = null;
+			prevoius.next= tmp;
 	}
-	 }	
 }
-	}
-	
-}
+ 
+
+
+
 
 public boolean Search(T val) {
-	current=head;
-	Node <T> tmp;
 	if(empty())
 		return false;
+	current=head;
+	Node <T> tmp;
 	while (current!=null && current.getData().compareTo(val)!=0)
 		current=current.getNext();
 	if(current.getData().compareTo(val)==0) {
 		tmp = current;
+		
 		return true;
 	}
+	current= head;
 return false;	
 }	
 	
+
+public void remove(T val) {
+	findfirst();
+	Node<T> previous = null ;
+	if (head.getData().compareTo(val)==0) // delete first
+		head = head.next;
+	else {
+		while(current.getData().compareTo(val)!=0) {
+			previous = current;
+			current = current.next;
+		}
+		  previous.setNext(current.next);
+		  current.setNext(null);
+	}
+	 if (current.next== null)
+		 current = head;
+	 else 
+		 current = current.next;
+}
+
+
+
+
+
+
+
+
+
 	
 	
 	
