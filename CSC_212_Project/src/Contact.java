b29f52a213d2b6ca37d7e339519d5da9898be843
SalehAD78 <145998115+SalehAD78@users.noreply.github.com>
2023-10-14 21:11:06 +0300

    Update Contact.java

--- CSC_212_Project/src/Contact.java
+++ CSC_212_Project/src/Contact.java
@@ -7,12 +7,22 @@ private String address;
 private int phone_number;
 private String email_address;
 Date birthday;
-LinkedList<Event>events;
+LinkedList <Event> event;
  
 
-
-
-public Contact(String name, String notes, String address, int phone_number, String email_address, Date birthday) {
+public Contact() {
+	this.address="";
+	this.name = "";
+	this.notes ="" ;
+	this.phone_number = 0;
+	this.email_address = "";
+	this.birthday = null;
+	this.event = new LinkedList<Event>();
+	
+}
+ 
+public Contact(String name, String notes, String address, int phone_number, String email_address, Date birthday,
+		LinkedList<Event> events) {
 	
 	this.name = name;
 	this.notes = notes;
@@ -20,80 +30,39 @@ public Contact(String name, String notes, String address, int phone_number, Stri
 	this.phone_number = phone_number;
 	this.email_address = email_address;
 	this.birthday = birthday;
+	this.event =new LinkedList<Event>();
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+public boolean addEvent(Event events) {
+	if(event.empty())
+		event.insertSortred(events);
+	else {
+		event.findfirst();
+	 for(int i = 0; i < event.length; i++ ) {
+		 if((event.retrive().date.compareTo(events.date)==0) && (event.retrive().time.compareTo(events.time)==0)) {
+			 return false;	 
+		 } 
+	 }	
+	}
+	 return true;
+}
+
+public boolean removeEvent(String EventTitle) {
+	if (event.empty())
+		return false;
+	Event newEvent = new Event();
+	newEvent.eventTitle = EventTitle;
+	  if(event.Search(newEvent)) {
+		event.remove(newEvent);	;
+		return true;
+	  }		
+	return false;	
+}
 
 
 public int compareTo(Contact o) {
 	
-	return 0;
+	return this.name.compareTo(o.name);
 }
 
 
