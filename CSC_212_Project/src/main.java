f3f900d59b46f829a3f4104a6a9696fbb82ac988
slemanone <146531001+slemanone@users.noreply.github.com>
2023-10-21 17:32:36 +0300

    Update main.java

--- CSC_212_Project/src/main.java
+++ CSC_212_Project/src/main.java
@@ -10,24 +10,6 @@ public class main   {
 		while( i == -1){
 			 i =p.start();
 		}
-	   //String x = "12/16/2003 2:40";
-	   //int minute = Integer.parseInt(x.split(" ")[1].split(":")[1]);
-	   
-		//System.out.println(minute+1);
-		//System.out.println(minute);
-		
-		//Contact x1 = new Contact("ahmed",05,"dksk@mkfm.com","aa","s","d");
-		//Contact x2 = new Contact("ali",35,"dksk@mkfm.com","aa","s","d");
-		//Contact x3 = new Contact("aseel",1,"dksk@mkfm.com","aa","s","d");
-		//Contact x4 = new Contact("bader",4,"dksk@mkfm.com","aa","s","d");
-		
-		//LinkedListOfContact fir = new LinkedListOfContact();
-		//fir.add(x1);
-		//fir.add(x2);
-		//fir.add(x3);
-		//fir.add(x4);
-		
-		//fir.printAll();
 	    	
 	}
 
